d7aa401eb79e57f052ff83530b4e926d834914fc
QBking <1521066881@qq.com>
2020-05-16 23:38:30 +0800

    附带‘回到顶部’按钮

--- navigation_bars/home.js
+++ navigation_bars/home.js
@@ -1,26 +1,48 @@
 $(document).ready(function(){
+
 	//导航栏标签下拉
-	$(".dropdown").on({
-		mouseover:function(){
-			$(this).addClass("open")
-		},
-		mouseout:function(){
-			$(this).removeClass("open")
+	$(".dropdown").hover(function(){
+		$(this).addClass("open")
+		var dropdownli=$('.dropdown>.dropdown-menu>li>').length,n=0;
+		function lirightin(){
+			$('.dropdown>.dropdown-menu>li:eq('+n+')').css('display','block')
+			n++;
+			if (n==dropdownli) {
+				n=0;
+				clearInterval(rightin);
+			}
 		}
+		rightin=setInterval(lirightin,80);
+	},
+	function(){
+		$(this).removeClass("open")
+		$('.dropdown>.dropdown-menu>li').css('display','none')
 	})
-	//导航栏自动隐藏
-	var t1=0,t2=0;
+
+	//导航栏自动隐藏及变色
+	var t1=0,t2=0,t3;
+	$(window).width()>991?t3=200:t3=50;
 	$(window).on('scroll',function(){
-		if($(window).scrollTop() > 350){
+		if($(window).scrollTop() > t3){
 			if (t1<0 || t2<0) {
-				$('.navbar').addClass('navhidden');
+				$('.navbar').addClass('navhidden').removeClass('ontop')
+				$('.scrolltop').addClass('in')
 			}
 			else{
-				$('.navbar').removeClass('navhidden');
+				$('.navbar').removeClass('navhidden').removeClass('ontop')
+				$('.navbar-header>a>img:eq(1)').fadeOut(200)//左端图片
+				$('.scrolltop').addClass('in')
 			}
 		}
 		else {
-			$('.navbar').removeClass('navhidden');
+			$('.navbar').removeClass('navhidden').addClass('ontop')
+			$('.scrolltop').removeClass('in')
+			if(t3==200){
+				$('.navbar-header>a>img:eq(1)').fadeIn(200)
+			}//仅限宽屏
+			else{
+				
+			}//仅限窄屏
 		}
 	})
 	var scrollFunc=function(e){
@@ -31,10 +53,12 @@ $(document).ready(function(){
 	        t2=e.detail;
 	    }
 	}
-	/*注册事件*/
-	if(document.addEventListener){
+	if(document.addEventListener){/*注册事件*/
 	    document.addEventListener('DOMMouseScroll',scrollFunc,false);
 	}//W3C
 	window.onmousewheel=document.onmousewheel=scrollFunc;//IE/Opera/Chrome
-	
+
+	//上滚到顶部事件
+	$('.scrolltop').click(function(){$('html,body').animate({scrollTop:0},300)})
+
 })
