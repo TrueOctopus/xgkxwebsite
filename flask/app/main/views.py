71e310557fc201b3d820315720309dcd6c652e96
trueoctopus <18368876370@163.com>
2020-05-18 18:02:14 +0800

    运动的云

--- flask/app/main/views.py
+++
@@ -1,7 +0,0 @@
-from flask import render_template, session, redirect, url_for
-from . import main
-
-
-@main.route('/', methods=['GET', 'POST'])
-def home():
-    return render_template('home.html')
